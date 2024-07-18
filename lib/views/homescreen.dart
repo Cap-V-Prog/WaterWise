7fc06772090504793977d5249b7be09ffe751029
mario <mario201santos11@gmail.com>
2024-07-18 18:00:03 +0100

    1.11

--- lib/views/homescreen.dart
+++ lib/views/homescreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatelessWidget {
   final VoidCallback onLogout;
@@ -8,7 +9,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         // Add your home screen content here
       ],
