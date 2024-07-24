56a290f2ebaca75d9c23a6c68642fdc8d2a8bd49
epicface59 <joaobatata59@gmail.com>
2024-07-24 20:42:01 +0100

    Settings completa.

--- lib/views/options_dialog.dart
+++ lib/views/options_dialog.dart
@@ -9,7 +9,8 @@ class OptionsDialog extends StatefulWidget {
 
 class _OptionsDialogState extends State<OptionsDialog> {
   final _goalController = TextEditingController();
-  double _currentGoal = 3.0;
+  final _emailController = TextEditingController();
+  double _currentGoal = 2.0;
   double _currentProgress = 0.0;
 
   @override
@@ -21,6 +22,7 @@ class _OptionsDialogState extends State<OptionsDialog> {
   Future<void> _loadCurrentGoalAndProgress() async {
     final user = FirebaseAuth.instance.currentUser;
     if (user != null) {
+      _emailController.text = user.email ?? '';
       final doc = await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
       if (doc.exists) {
         final data = doc.data();
@@ -37,6 +39,7 @@ class _OptionsDialogState extends State<OptionsDialog> {
   @override
   void dispose() {
     _goalController.dispose();
+    _emailController.dispose();
     super.dispose();
   }
 
@@ -55,38 +58,254 @@ class _OptionsDialogState extends State<OptionsDialog> {
     }
   }
 
+  Future<void> _showChangePasswordDialog() async {
+    final _currentPasswordController = TextEditingController();
+    final _newPasswordController = TextEditingController();
+
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Mudar senha'),
+          content: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextField(
+                  controller: _currentPasswordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Senha atual',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  controller: _newPasswordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Nova senha',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancelar', style: TextStyle(color: Colors.red),),
+            ),
+            TextButton(
+              onPressed: () async {
+                final user = FirebaseAuth.instance.currentUser;
+                if (user != null) {
+                  try {
+                    if (_newPasswordController.text.length < 6) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('A nova senha deve ter pelo menos 6 caracteres')),
+                      );
+                      return;
+                    }
+                    final credential = EmailAuthProvider.credential(
+                      email: user.email ?? '',
+                      password: _currentPasswordController.text,
+                    );
+
+                    await user.reauthenticateWithCredential(credential);
+                    await user.updatePassword(_newPasswordController.text);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Senha atualizada com sucesso')),
+                    );
+                    Navigator.of(context).pop();
+                  } catch (e) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Erro ao atualizar senha: $e')),
+                    );
+                  }
+                }
+              },
+              child: const Text('Confirmar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _showAccountDialog() async {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Conta',style: TextStyle(color:Colors.white)),
+
+          backgroundColor: Color(0xFF5B8ADB),
+          surfaceTintColor: Colors.transparent,
+          content: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: "Email",
+                    hintStyle: const TextStyle(color:Colors.white),
+                  ),
+                  enabled: false, // Disable editing email
+
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  controller: TextEditingController(text: '******'),
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: "Senha",
+                    hintStyle: const TextStyle(color: Colors.white),
+                  ),
+                  enabled: false, // Disable editing password
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: _showChangePasswordDialog,
+                  child: Text('Mudar senha'),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Fechar',style: TextStyle(color: Colors.white)),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _showDailyGoalDialog() async {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Color(0xFF5B8ADB),
+          title: const Text('Definir o objetivo diário', style: TextStyle(color: Colors.white),),
+          content: TextField(
+            controller: _goalController,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide: BorderSide.none,
+              ),
+              fillColor: Colors.white,
+              filled: true,
+              hintText: "Objetivo diário em (L)",
+              hintStyle: const TextStyle(color: Color(0xFF5B8ADB)),
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancelar', style: TextStyle(color: Colors.red),),
+            ),
+            TextButton(
+              onPressed: () async {
+                await _saveDailyGoal();
+              },
+              child: const Text('Guardar', style: TextStyle(color: Colors.white),),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      title: const Text('Definir o objetivo diário',style: TextStyle(color: Color(0xFF5B8ADB)),),
-      content: TextField(
-        controller: _goalController,
-        keyboardType: TextInputType.number,
-        decoration: InputDecoration(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10.0),
-            borderSide: BorderSide.none,
-          ),
-          fillColor: Color(0xFF5B8ADB),
-          filled: true,
-          hintText: "Objetivo diário em (L)",
-          hintStyle: const TextStyle(color: Colors.white),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Configurações'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            GestureDetector(
+              onTap: _showAccountDialog,
+              child: Container(
+                padding: EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color:  Color(0xFF5B8ADB),
+                  border: Border.all(color: Colors.transparent),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Conta',
+                      style: const TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Icon(
+                      Icons.arrow_forward_ios,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            GestureDetector(
+              onTap: _showDailyGoalDialog,
+              child: Container(
+                padding: EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Color(0xFF5B8ADB),
+                  border: Border.all(color: Colors.transparent),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Objetivo diário',
+                      style: const TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Icon(
+                      Icons.arrow_forward_ios,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: const Text('Cancelar',style: TextStyle(color: Colors.red),),
-        ),
-        TextButton(
-          onPressed: () async {
-            await _saveDailyGoal();
-          },
-          child: const Text('Guardar'),
-        ),
-      ],
     );
   }
 }
